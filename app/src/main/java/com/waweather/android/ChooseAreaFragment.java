43c32428ec4fef358c91f6c610e9d6320f5c02c7
yueguangshuian <1327603199@qq.com>
2020-03-13 10:49:29 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/com/waweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/waweather/android/ChooseAreaFragment.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
 import com.waweather.android.db.City;
 import com.waweather.android.db.County;
 import com.waweather.android.db.Province;
+import com.waweather.android.gson.Weather;
 import com.waweather.android.util.HttpUtil;
 import com.waweather.android.util.Utility;
 
@@ -113,11 +114,18 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 } else if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(), WeatherActivity.
-                            class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.
+                                class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
